63c1d4fca848650168efc221cef0b5fa16d7673c
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-16 11:03:18 +0700

    Menambahkan Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Navbar from "./components/navbar"
 import Contoh from "./pages/contoh"
 
 function App() {
   return (
     <>
-      <h1 className=" text-lg font-bold text-red-500">Hello World!!</h1>
-      <Contoh />
+    <Navbar />
     </>
   )
 }
