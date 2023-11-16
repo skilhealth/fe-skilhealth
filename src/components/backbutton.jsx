bce60f8c22719e3e55d922f5c349e28d671f660c
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-16 16:59:39 +0700

    menambahkan back-button

---
+++ src/components/backbutton.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom"
+import backicon from "../assets/back-button.png"
+
+function Backbutton({nama}) {
+    return (
+        <div className="w-full h-8 item-center align-baseline flex text-xl font-bold gap-3 mb-4">
+        <button><img src={backicon} alt="" className="h-full"/></button>
+        {nama}
+    </div>
+    )
+}
+
+export default Backbutton
