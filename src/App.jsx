dd5e1781226d2c8b7d598cafc90a2deb637faf88
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-16 11:45:10 +0700

    add backbutton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Backbutton from "./components/backbutton"
 import Contoh from "./pages/contoh"
 
 function App() {
   return (
     <>
-      <h1 className=" text-lg font-bold text-red-500">Hello World!!</h1>
-      <Contoh />
+      <Backbutton nama="Halaman Saya" />
     </>
   )
 }
