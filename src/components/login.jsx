66f6b6aa94213cf331f3579fc83a0aaddb0c75fb
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-16 20:18:47 +0700

    membuat login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -9,39 +9,39 @@ function Login () {
                     <div className="w-full lg:w-6/12 px-4">
                             <div className="rounded-t mb-0 px-6 py-6">
                                 <div className="text-center mb-3">
-                                    <h1 className=" font-sans text-black-600 text-sm font-bold">
+                                    <h1 className=" text-2xl font-sans font-bold">
                                         Selamat Datang <br></br> di SkilHealth !
                                     </h1>
                                 </div>
                                 <div className="btn-wrapper text-center">
-                                        <img alt="..." className="ml-10 flex w-40 h-40" src= {imgSkilhealth} />
+                                        <img alt="..." className="ml-16 flex w-40 h-40" src= {imgSkilhealth} />
                                 </div>
                             </div>
                             <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                                 <form id='form-login' autoComplete='off'>
                                     <div className="relative w-full mb-3">
-                                        <label className="block uppercase text-black-700 text-xs font-bold mb-2" htmlFor="grid-password">Username / Email</label>
-                                        <input type="email" className="px-4 py-4 placeholder-gray-400 text-gray-800 bg-white rounded-xl text-sm shadow focus:outline-black-200 focus:shadow-outline- w-full" placeholder="Masukan User/Email" autoFocus required />
+                                        <label className="block font-sans text-lg font-bold mb-2" htmlFor="grid-password">Username / Email</label>
+                                        <input type="email" className="px-4 py-4 placeholder-gray-400 bg-white rounded-xl text-sm shadow focus:outline-black-200 focus:shadow-outline- w-full" placeholder="Masukan Username/Email" autoFocus required />
                                     </div>
                                     <div className="relative w-full mb-3">
-                                        <label className="block uppercase text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">Password</label>
-                                        <input type="password" className="px-4 py-4 placeholder-gray-400 text-gray-800 bg-white rounded-xl  border-inherit text-sm shadow focus:outline-black-200 focus:shadow-outline w-full" placeholder="Masukan Password" required />
+                                        <label className="block font-sans text-lg font-bold mb-2" htmlFor="grid-password">Password</label>
+                                        <input type="password" className="px-4 py-4 placeholder-gray-400 bg-white rounded-xl  border-inherit text-sm shadow focus:outline-black-200 focus:shadow-outline w-full" placeholder="Masukan Password" required />
                                     </div>
                                     <div>
                                         <label className="inline-flex items-center cursor-pointer">
                                             <input id="customCheckLogin" type="checkbox" className="form-checkbox text-gray-800 ml-1 w-5 h-5 ease-linear transition-all duration-150" defaultChecked />
-                                            <span className="ml-2 text-sm font-semibold text-black-700">Ingat Saya</span>
-                                            <a href= "" className="text-end ">Belum Punya Akun?</a>
+                                            <span className="ml-2 text-lg font-semibold text-black-700">Ingat Saya</span>
+                                            <a href= "" className="text-lg ml-16">Ubah Password</a>
                                         </label>
                                     </div>
                                     <div className="text-center mt-6">
-                                        <button className="bg-red-700 text-white text-sm font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150">
+                                        <button className="bg-red-700 text-white text-sm font-bold px-6 py-3 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150">
                                             Login
                                         </button>
                                     </div>
+                                    <a href= "" className="text-lg ml-40">Belum Punya Akun?</a>
                                 </form>
                             </div>
-                        
                     </div>
                 </div>
             </div>
