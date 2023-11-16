ee09509506ed2d04d93d1fc4ef00689e7f1fcd8d
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-16 15:14:59 +0700

    Menambah Doctor Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import Contoh from "./pages/contoh"
-
 function App() {
   return (
     <>
-      <h1 className=" text-lg font-bold text-red-500">Hello World!!</h1>
-      <Contoh />
     </>
   )
 }
