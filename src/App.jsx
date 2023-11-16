a8f7d94ebd439a36d29e9752fab7b8bc12668c76
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-16 19:46:43 +0700

    membuat login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Backbutton from "./components/backbutton"
+import Login from "./components/login"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
 
     <Backbutton/>
+    <Login/>
 
     </>
   )
