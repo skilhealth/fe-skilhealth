8312854562754c47c938b43a3ab44f6f414b298c
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-17 08:43:39 +0700

    Create UI Detail Dokter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import Navbar from "./components/navbar"
+import DataDokter from "./pages/data-doctor-page"
+
 function App() {
   return (
     <div>
+      <Navbar />
+      <DataDokter />
     </div>
   )
 }
