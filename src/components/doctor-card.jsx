6fb20e926fc5b6e810021c8b554f044959b5b129
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-18 20:12:42 +0700

    menambabhkan card

---
+++ src/components/doctor-card.jsx
@@ -0,0 +1,20 @@
+
+
+function hospitalCard() {
+    return (
+        <div className="card">
+        <div className="">
+            <div className="container">
+                <div className="bg-"></div>
+                <div className=""></div>
+                <h2 className=""></h2>
+                <div className=""></div>
+                <div className=""></div>
+            </div>
+        </div>
+    </div>
+    )
+  
+}
+
+export default hospitalCard
