d9b541f9e3ec8aafa97a298933b3f90412e45f75
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-19 03:35:51 +0700

    membuat menu profile belum selesai

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "./components/navbar"
 import MenuProfile from "./pages/menu-profile-page"
+import ProfilePasien from "./pages/profil-pasien"
 
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
-    <MenuProfile></MenuProfile>
+    <ProfilePasien></ProfilePasien>
     </>
   )
 }
---
+++ src/pages/profil-pasien.jsx
@@ -0,0 +1,15 @@
+
+import Backbutton from "../components/backbutton";
+
+function ProfilePasien () {
+     return (
+      <div>
+           <Backbutton nama= "Profil Pasien" />
+           <h1 className="font-sans font-bold mt-6">Saya Sendiri</h1>
+      </div>
+      
+        
+     )
+}
+
+export default ProfilePasien
