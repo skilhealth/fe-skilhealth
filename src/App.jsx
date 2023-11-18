5ae79c57f6db22271ea278985c5a7e6898508229
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-19 03:23:31 +0700

    membuat menu profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import Navbar from "./components/navbar"
+import MenuProfile from "./pages/menu-profile-page"
+
+
 function App() {
   return (
     <>
+    <Navbar></Navbar>
+
+    <MenuProfile></MenuProfile>
     </>
   )
 }
---
+++ src/pages/menu-profile-page.jsx
@@ -0,0 +1,20 @@
+
+import Backbutton from "../components/backbutton";
+
+function MenuProfile () {
+     return (
+      <div>
+           <Backbutton nama= "Profil saya" />
+           <ul className="w-full flex-col px-4 absolute list-none" >
+               <li className="py-4 w-full text-left border-black-200 border-b-2 font-semibold"><a href="pages/profil-pasien.jsx">Profile pasien</a></li>
+               <li className="py-4 w-full text-left border-black-200 border-b-2 font-semibold"><a href="#">Bahasa</a></li>
+               <li className="py-4 w-full text-left border-black-200 border-b-2 font-semibold"><a href="#">Pusat Bantuan</a></li>
+               <li className="py-4 w-full text-left border-black-200 border-b-2 font-semibold"><a href="#">Keluar</a></li>
+           </ul> 
+      </div>
+      
+        
+     )
+}
+
+export default MenuProfile
