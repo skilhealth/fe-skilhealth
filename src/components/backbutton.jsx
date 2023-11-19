9668692c72660176dc8c34480ccd30878bbdccea
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-19 20:17:53 +0700

    navigasi Back Button

--- src/components/backbutton.jsx
+++ src/components/backbutton.jsx
@@ -2,9 +2,10 @@ import { useNavigate } from "react-router-dom"
 import backicon from "../assets/back-button.png"
 
 function Backbutton({nama}){
+    const history = useNavigate()
     return( 
         <div className="w-full h-8 item-center align-baseline flex text-xl font-bold gap-3 mb-4">
-            <button><img src={backicon} alt="" className="h-full"/></button>
+            <button onClick={()=>history(-1)}><img src={backicon} alt="" className="h-full"/></button>
             {nama}
         </div>
     )
