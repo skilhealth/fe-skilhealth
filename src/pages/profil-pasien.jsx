b16cd1471f68746c696e8b1b0aa0c39a6c9bb22e
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-19 21:56:51 +0700

    membuat profile

--- src/pages/profil-pasien.jsx
+++ src/pages/profil-pasien.jsx
@@ -1,11 +1,20 @@
 
 import Backbutton from "../components/backbutton";
+import ProfileCard from "../components/profile-card";
 
 function ProfilePasien () {
      return (
       <div>
            <Backbutton nama= "Profil Pasien" />
-           <h1 className="font-sans font-bold mt-6">Saya Sendiri</h1>
+           <h1 className="font-sans font-bold mt-5 ml-5">Saya Sendiri</h1>
+           <ProfileCard nama="Andrew Lee - Saya Sendiri" tgl_lahir= "15 September 2003" no_telepon= "+62881234454367"></ProfileCard>
+           <h1 className="font-sans font-bold mt-5 ml-5">Orang Lain</h1>
+           <button
+            type="submit"
+            className='bg-red-700 text-white text-sm font-bold px-3 py-3 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mt-96 ml-24 w-52  ease-linear transition-all duration-150 sm:text-sm'
+          >
+            Tambah Profil Lain
+          </button>
       </div>
       
         
