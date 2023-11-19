60904830a16bd24bcde26b87dca7ba187ace568f
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-19 22:25:55 +0700

    UI list Uji lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/navbar"
 import HasilCariPage from "./pages/hasil-pencarian-page"
+import ListUjiLab from "./pages/list-ujilab"
 import PencarianDokter from "./pages/pencarian-dokter"
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/doctor/search" element={<PencarianDokter />} />
+        <Route path="/ujilab/" element={<ListUjiLab />} />
         <Route path="/doctor/result" element={<HasilCariPage />} />
         <Route path="/doctor/result/:id" element={<HasilCariPage />} />
       </Routes>
