c9416ffd1b7f1593557ec1ccf0c0f824c2c1640d
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-19 20:03:45 +0700

    atur router

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import ListBooking from "./pages/list-booking-page"
 import PencarianDokter from "./pages/pencarian-dokter"
 import Pembayaran from "./pages/pembayaran-page"
 import CheckIn from "./pages/checkin-page"
+import DataDokter from "./pages/data-doctor-page"
+import MenungguPanggilan from "./pages/menunggu-panggilan-page"
+import Homecare from "./pages/homecare-page"
+import Editbooking from "./pages/editbooking"
+import Refund from "./pages/refund-page"
 
 function App() {
   return (
@@ -13,9 +18,16 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/doctor/search" element={<PencarianDokter />} />
-        <Route path="/doctor/result" element={<HasilCariPage />} />
-        <Route path="/doctor/result/:id" element={<HasilCariPage />} />
+        <Route path="/doctor/" element={<HasilCariPage />} />
+        <Route path="/doctor/:id" element={<DataDokter />} />
+        <Route path="/booking/pembayaran" element={<Pembayaran />} />
+        <Route path="/booking/" element={<ListBooking />} />
         <Route path="/booking/:id" element={<BookingDetailPage />} />
+        <Route path="/booking/:id/luring" element={<CheckIn />} />
+        <Route path="/booking/:id/daring" element={<MenungguPanggilan />} />
+        <Route path="/booking/:id/homecare" element={<Homecare />} />
+        <Route path="/booking/:id/edit" element={<Editbooking />} />
+        <Route path="/booking/:id/refund" element={<Refund />} />
       </Routes>
       
     </>
