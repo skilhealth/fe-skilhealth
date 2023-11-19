7be8c01d2e1e52c6be1b9265de8d46eef7a7001f
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-19 15:29:31 +0700

    UI refund

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/navbar"
 import HasilCariPage from "./pages/hasil-pencarian-page"
 import PencarianDokter from "./pages/pencarian-dokter"
+import Refund from "./pages/refund-page"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/doctor/search" element={<PencarianDokter />} />
         <Route path="/doctor/result" element={<HasilCariPage />} />
         <Route path="/doctor/result/:id" element={<HasilCariPage />} />
+        <Route path="/booking/:id/refund" element={<Refund />} />
       </Routes>
       
     </>
---
+++ src/pages/refund-page.jsx
@@ -0,0 +1,25 @@
+import Backbutton from "../components/backbutton"
+
+function Refund(){
+    return(
+        <div className="p-4">
+            <Backbutton nama="Batalkan Jadwal" />
+            <form className="bg-slate-200 w-full p-4 rounded-lg">
+                <div className="text-lg mb-4">
+                    <div className="font-bold">Alasan Pembatalan</div>
+                    <select name="" id="" className="w-full p-2 border-2 rounded-lg border-black">
+                        <option value="">Pilih Alasan Pembatalan</option>
+                    </select>
+                </div>
+                <div className="text-lg mb-4">
+                    <div className="font-bold">Nomor Rekening Bank</div>
+                    <input name="" id="" className="w-full p-2 border-2 rounded-lg border-black" placeholder="Masukan Nomor Rekening" />
+                </div>
+                <div className="w-full p-4">
+                    <button className="w-full bg-red-700 text-white p-2 rounded-lg">Konfirmasi</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+export default Refund
