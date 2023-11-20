9fa3f904a2145e11981e2b8c858d8124ce8b45df
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-20 15:36:26 +0700

    membuat tingkat biasa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar"
 import MenuAmbulance from "./pages/menu-ambulance"
+import TingkatBiasa from "./pages/tingkat-biasa"
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
-    <MenuAmbulance></MenuAmbulance>
+    <TingkatBiasa></TingkatBiasa>
+
     </>
   )
 }
---
+++ src/pages/tingkat-biasa.jsx
@@ -0,0 +1,12 @@
+import Backbutton from "../components/backbutton";
+
+
+function TingkatBiasa () {
+    return (
+        <div>
+             <Backbutton nama= "Biasa" />
+        </div>
+    )
+}
+
+export default TingkatBiasa
