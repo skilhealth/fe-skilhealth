7632028d3e8901bc8f4e168a48b775fa1eb2cab0
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-20 22:04:59 +0700

    membuat biasa

--- src/pages/tingkat-biasa.jsx
+++ src/pages/tingkat-biasa.jsx
@@ -6,10 +6,10 @@ function TingkatBiasa () {
     return (
         <div>
              <Backbutton nama= "Biasa" />
-             <div className="px-10 py-10 bg-gray-100 rounded-lg mt-10 sm:text-sm" >
+             <div className=" w-full h-32 px-4 py-2 bg-gray-100 rounded-lg mt-10 sm:text-sm" >
                 <div>
                     <img className="w-20 h-20" src={imgambulancebiasa}></img>
-                    <h5 className="font-lg font-bold">Biasa</h5>
+                    <h5 className="font-lg font-bold ml-5">Biasa</h5>
                 </div>
              
              </div>
