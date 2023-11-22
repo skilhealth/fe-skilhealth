71e431a9284cf4459cc8708c9826e54bdd64a297
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-22 10:53:19 +0700

    data dinmasi

--- src/components/doctor-card.jsx
+++ src/components/doctor-card.jsx
@@ -1,19 +1,17 @@
-import imgdoctor from "../assets/doctor1.png"
-
-function DoctorCard() {
+function DoctorCard({data}) {
     return (
         <div className="bg-slate-200 w-full h-40 flex p-5 gap-3">
             <div className="h-full">
-                <img src={imgdoctor} alt="" className="h-full rounded-xl object-cover" />
+                <img src={`${data.img}`} alt="" className="h-full rounded-xl object-cover" />
             </div>
             <div className="flex flex-col jus">
                 <div className="flex items-center gap-2">
                     <div className="w-4 h-4 rounded-full bg-green-500"></div>
-                    Sedang di Rumah Sakit
+                    {data.status}
                 </div>
-                <div className="text-xl font-bold">Dr. Jaydon Schleifer</div>
-                <div className="text-base font-medium">Ahli Jantung</div>
-                <div className="text-base font-regular">Skilvul Hospital Jakarta</div>
+                <div className="text-xl font-bold">{data.nama}</div>
+                <div className="text-base font-medium">{data.spesialis}</div>
+                <div className="text-base font-regular">{data.rs}</div>
             </div>
         </div>
     )
--- src/pages/homecare-page.jsx
+++ src/pages/homecare-page.jsx
@@ -1,11 +1,29 @@
 import Backbutton from "../components/backbutton"
 import DoctorCard from "../components/doctor-card"
+import 'moment/locale/id'
+import moment from "moment/moment";
 
-function Homecare(){
-    return(
+
+function Homecare() {
+    moment.locale('id');
+    const data = {
+        token: "GH651H",
+        antrian: "JT02",
+        sisa: 2,
+        date: moment("2023-12-12"),
+        dokter:
+        {
+            status: "Sedang Dirumah Sakit",
+            img: "/images/dokter/doctor1.png",
+            nama: "Dr. Jaydon Schleifer",
+            spesialis: "Ahli Jantung",
+            rs: "Skilvul Hospital jakarta"
+        },
+    }
+    return (
         <div className="p-4">
             <Backbutton nama="Status Dokter" />
-            <DoctorCard />
+            <DoctorCard data={data.dokter} />
         </div>
     )
 }
