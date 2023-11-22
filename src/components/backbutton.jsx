c10b1dd2290ed199aa87878419f769fe8ca8c0bd
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-23 04:43:38 +0700

    membuat desktop

--- src/components/backbutton.jsx
+++ src/components/backbutton.jsx
@@ -4,7 +4,7 @@ import backicon from "../assets/back-button.png"
 function Backbutton({nama}){
     return( 
         <div className="w-full h-8 item-center align-baseline flex text-xl font-bold gap-3 mb-4">
-            <button><img src={backicon} alt="" className="h-full"/></button>
+            <button><img src={backicon} alt="" className="h-full lg:hidden"/></button>
             {nama}
         </div>
     )
--- src/components/rs-card.jsx
+++ src/components/rs-card.jsx
@@ -2,7 +2,7 @@
  
  function HospitalCard ({jarak, name, area, alamat}) {
     return (
-        <div className='px-5 py-5 bg-gray-100 flex rounded-md shadow-sm mt-8 space-y-px border border-gray-200 hover:border-r-5 hover:border-b-5 hover:border-gray-500 transform hover:scale-101 transition duration-300 w-full focus:z-10 sm:px-6 lg:px-8'>
+        <div className='px-5 py-5 bg-gray-100 flex rounded-md shadow-sm mt-8 space-y-px border border-gray-200 hover:border-r-5 hover:border-b-5 hover:border-gray-500 transform hover:scale-101 transition duration-300 w-full focus:z-10 sm:px-6 lg:px-6'>
         <img className='w-26 h-28 border mt-5' src={imghospital} alt=""  />
             <div className=' relative flex flex-col space-y-4 justify-between items-start font-sans left-4 sm:text-sm' >
                 <div className=''>{jarak}</div>
