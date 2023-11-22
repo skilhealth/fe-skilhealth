4bc13398a570615aa995a994beb4bec1a7fde368
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-22 07:45:45 +0700

    Data Dinamis

--- src/components/jadwal-dokter.jsx
+++ src/components/jadwal-dokter.jsx
@@ -1,38 +1,25 @@
 import { useState } from "react"
 
-function JadwalDokter() {
+function JadwalDokter({jadwal,useharga}) {
     const [selectOption, setoption] = useState("")
-    const handleOption = (id) => {
+    const handleOption = (id,harga) => {
         setoption(id)
+        useharga(harga)
     }
     return (
         <div className="bg-slate-200 w-full mt-2 p-2">
             <h3 className="text-lg font-bold mb-2">Jadwal Praktik</h3>
             <div className="flex text-xs justify-between">
-                <label className={`flex flex-col w-max rounded-md p-2 border-2 ${(selectOption === 1) ? "border-red-700 bg-red-700 text-white" : "border-black"}`}>
-                    <input type="radio" name="jadwal" value={"senin"} className="hidden" onChange={() => handleOption(1)} />
-                    <div className="">Regular</div>
-                    <div className="">Selasa,</div>
-                    <div className="">12-12-2023</div>
-                </label>
-                <label className={`flex flex-col w-max rounded-md p-2 border-2 ${(selectOption === 2) ? "border-red-700 bg-red-700 text-white" : "border-black"}`}>
-                    <input type="radio" name="jadwal" value={"senin"} className="hidden" onChange={() => handleOption(2)} />
-                    <div className="">Regular</div>
-                    <div className="">Selasa,</div>
-                    <div className="">12-12-2023</div>
-                </label>
-                <label className={`flex flex-col w-max rounded-md p-2 border-2 ${(selectOption === 3) ? "border-red-700 bg-red-700 text-white" : "border-black"}`}>
-                    <input type="radio" name="jadwal" value={"senin"} className="hidden" onChange={() => handleOption(3)} />
-                    <div className="">Regular</div>
-                    <div className="">Selasa,</div>
-                    <div className="">12-12-2023</div>
-                </label>
-                <label className={`flex flex-col w-max rounded-md p-2 border-2 ${(selectOption === 4) ? "border-red-700 bg-red-700 text-white" : "border-black"}`}>
-                    <input type="radio" name="jadwal" value={"senin"} className="hidden" onChange={() => handleOption(4)} />
-                    <div className="">Regular</div>
-                    <div className="">Selasa,</div>
-                    <div className="">12-12-2023</div>
-                </label>
+                {
+                    jadwal.map((item,index) => (
+                        <label key={index} className={`flex flex-col w-max rounded-md p-2 border-2 ${(selectOption === index) ? "border-red-700 bg-red-700 text-white" : "border-black"}`}>
+                            <input type="radio" name="jadwal" value={"senin"} className="hidden" onChange={() => handleOption(index,item.harga)} />
+                            <div className="">{item.tipe}</div>
+                            <div className="">{item.hari},</div>
+                            <div className="">{item.tgl}</div>
+                        </label>
+                    ))
+                }
             </div>
         </div>
     )
