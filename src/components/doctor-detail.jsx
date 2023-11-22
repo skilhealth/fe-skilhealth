1389ce6ab7e80f8db4066295cda3c7eeef186561
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-22 11:11:50 +0700

    data dinmasi

--- src/components/doctor-detail.jsx
+++ src/components/doctor-detail.jsx
@@ -1,42 +1,44 @@
-import imgdoctor from "../assets/doctor1.png"
 
-function DoctorDetail() {
+
+function DoctorDetail({ data }) {
     return (
         <div className="w-full bg-slate-200">
             <div className="bg-slate-200 w-full h-40 flex p-5 gap-3">
                 <div className="h-full">
-                    <img src={imgdoctor} alt="" className="h-full rounded-xl object-cover" />
+                    <img src={`${data.img}`} alt="" className="h-full rounded-xl object-cover" />
                 </div>
                 <div className="flex flex-col jus">
                     <div className="flex items-center gap-2">
                         <div className="w-4 h-4 rounded-full bg-green-500"></div>
-                        Sedang di Rumah Sakit
+                        {data.rs}
                     </div>
-                    <div className="text-xl font-bold">Dr. Jaydon Schleifer</div>
-                    <div className="text-base font-medium">Ahli Jantung</div>
-                    <div className="text-base font-regular">Skilvul Hospital Jakarta</div>
+                    <div className="text-xl font-bold">{data.nama}</div>
+                    <div className="text-base font-medium">{data.spesialis}</div>
+                    <div className="text-base font-regular">{data.rs}</div>
                 </div>
             </div>
             <div className="px-4 pb-4">
                 <div className="mb-2">
                     <h3 className="font-bold">Tentang Dokter</h3>
-                    <p className="text">Dr. Jaydon Schleifer adalah seorang spesialis jantung berpengalaman dengan lebih dari
-                        15 tahun praktik medis.Saat ini Beliau praktik di skilvul Hospital Jakarta.</p>
+                    <p className="text">{data.keterangan}</p>
                 </div>
                 <div className="mb-2">
                     <h3 className="font-bold">Nomor SKD</h3>
-                    <p className="text">10297361028361</p>
+                    <p className="text">{data.skd}</p>
                 </div>
                 <div className="mb-2">
                     <h3 className="font-bold">Pengalaman Praktek</h3>
                     <ul className="px-4">
-                        <li className="list-disc">RSUD ANU</li>
-                        <li className="list-disc">RSUD ANU</li>
+                        {data.pengalaman.map((item) => (
+                            <li className="list-disc">{item}</li>
+                        ))}
                     </ul>
                 </div>
                 <div className="mb-2">
                     <h3 className="font-bold">Pendidikan Terakhir</h3>
-                    <p className="text">Universitas Kedokteran</p>
+                    {data.pendidikan.map((item) => (
+                            <li className="list-disc">{item}</li>
+                        ))}
                 </div>
             </div>
         </div>
--- src/pages/editbooking.jsx
+++ src/pages/editbooking.jsx
@@ -1,14 +1,49 @@
+import moment from "moment/moment"
 import Backbutton from "../components/backbutton"
 import DoctorDetail from "../components/doctor-detail"
 import JadwalDokter from "../components/jadwal-dokter"
 
 function Editbooking() {
-
+    const {data} = {
+        data: {
+            nama: "Dr. Jaydon Schleifer",
+            status: "Sedang Dirumah Sakit",
+            spesialis: "Ahli Jantung",
+            rs: "Skilvul Hospital Jakarta",
+            keterangan: "Dr. Jaydon Schleifer adalah seorang spesialis jantung berpengalaman dengan lebih dari 15 tahun praktik medis.Saat ini Beliau praktik di skilvul Hospital Jakarta.",
+            skd: 10297361028361,
+            pengalaman: ["RSD UNI", "RSU ANU"],
+            pendidikan: ["Universitas Singaperbangsa Karawang"],
+            img: "/images/dokter/doctor1.png",
+            jadwal: [{
+                hari: "Senin",
+                tgl: moment("2023-12-14").format("DD-MM-YYYY"),
+                tipe: "Regular",
+                harga: 60000
+            }, {
+                hari: "Rabu",
+                tgl: moment("2023-12-15").format("DD-MM-YYYY"),
+                tipe: "Homecare",
+                harga: 80000
+            },
+            {
+                hari: "Sabtu",
+                tgl: moment("2023-12-17").format("DD-MM-YYYY"),
+                tipe: "Daring",
+                harga: 30000
+            }, {
+                hari: "Minggu",
+                tgl: moment("2023-12-18").format("DD-MM-YYYY"),
+                tipe: "Regular",
+                harga: 60000
+            }]
+        }
+    }
     return (
         <div className="p-4">
             <Backbutton nama="Atur Jadwal Kembali" />
-            <DoctorDetail />
-            <JadwalDokter />
+            <DoctorDetail data={data} />
+            <JadwalDokter jadwal={data.jadwal.filter(schdule=>schdule.tipe==='Regular')} />
             <div className="flex px-6 mt-2 w-full border-slate-100 p-2 justify-between gap-2">
                 <button className="w-full bg-red-700 p-2 rounded-lg text-white ">Konfirmasi</button>
             </div>
