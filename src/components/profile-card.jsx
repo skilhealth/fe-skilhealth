67d22b556c9ff76fea0c6227ad441c6d0c4c79a5
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-23 04:07:16 +0700

    membuat desktop

--- src/components/profile-card.jsx
+++ src/components/profile-card.jsx
@@ -1,7 +1,7 @@
 
 function ProfileCard ({nama, tgl_lahir, no_telepon}) {
     return (
-        <div className="px-5 py-5 bg-gray-100 flex rounded-md shadow-sm mt-2 space-y-6 border border-gray-200 hover:border-r-5 hover:border-b-5 hover:border-gray-500 transform hover:scale-101 transition duration-300 lg:w-2/4 item-center">  
+        <div className="px-5 py-5 bg-gray-100 flex rounded-md shadow-sm mt-2 space-y-6 border border-gray-200 hover:border-r-5 hover:border-b-5 hover:border-gray-500 transform hover:scale-101 transition duration-300 lg:m-7 justify-items-center">  
         <div className="relative flex flex-col space-y-4 justify-between items-start font-sans bottom-2 right-2">
                 <div className=' font-bold'>{nama}</div>
                 <h1 className=''>{tgl_lahir}</h1>
