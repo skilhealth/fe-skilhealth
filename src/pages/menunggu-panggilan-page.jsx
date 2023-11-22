a04c30a10449cd4d08ed2df8dee00e8bff3bc4ea
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-22 10:41:09 +0700

    data dinamis

--- src/pages/menunggu-panggilan-page.jsx
+++ src/pages/menunggu-panggilan-page.jsx
@@ -1,18 +1,36 @@
+import moment from "moment/moment"
 import Backbutton from "../components/backbutton"
+import 'moment/locale/id'
 
 function MenungguPanggilan() {
+    moment.locale('id');
+    const data = {
+        token: "GH651H",
+        antrian: "JT02",
+        sisa: 2,
+        date: moment("2023-12-12"),
+        dokter:
+        {
+            status: "Sedang Dirumah Sakit",
+            img: "/images/dokter/doctor1.png",
+            nama: "Dr. Jaydon Schleifer",
+            spesialis: "Ahli Jantung",
+            rs: "Skilvul Hospital jakarta"
+        },
+    }
+    const hari = data.date
     return (
         <div className="p-4">
             <Backbutton nama="Menunggu Panggilan" />
             <div className=" bg-slate-200 w-full font-medium p-4 rounded-lg flex flex-col">
                 <div className=" border-b-4 border-slate-300 py-2 mb-2">
-                    Hari Jumat, 15-09-2023 ,Jam 20:00 WIB
+                    {`Hari ${data.date.format('dddd')}, ${data.date.format('DD-MM-YYYY')}, Jam 20:00 WIB`}
                 </div>
                 <div className="mb-10">
                     <h2 className="text-2xl font-bold">
-                        Dr. Jaydon Schleifer
+                        {data.dokter.nama}
                     </h2>
-                    <div>Ahli Jantung</div>
+                    <div>{data.dokter.spesialis}</div>
                 </div>
                 <div className="w-full">
                     Dokter Sudah berada dipanggilan
