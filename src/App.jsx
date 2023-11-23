43e04452efe60f04a405157ac7bd70d53d0f9563
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-23 18:15:52 +0700

    membuat menu dokter mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar"
+import MenuProfileDokter from "./pages/menu-profile-dokter"
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
+    <MenuProfileDokter></MenuProfileDokter>
     </>
   )
 }
