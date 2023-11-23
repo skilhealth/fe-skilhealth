a3256d614a7c2553d6c855e6e11b90d9ff796163
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-23 17:39:56 +0700

    membuat desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import Navbar from "./components/navbar"
 import EditProfile from "./pages/edit-profile"
-import EditProfileDokter from "./pages/edit-profile-dokter"
-import MenuProfileDokter from "./pages/menu-profile-dokter"
 import MenuProfile from "./pages/menu-profile-page"
-import ProfilePasien from "./pages/profil-pasien"
-import TambahProfile from "./pages/tambah-profile"
+
+
 
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
-    <EditProfileDokter></EditProfileDokter>
 
     </>
   )
