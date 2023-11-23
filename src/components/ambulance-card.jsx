f5a2aa98ff3bd5dcad4eb02b22cc035175fe2292
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-23 11:24:49 +0700

    membuat desktop

--- src/components/ambulance-card.jsx
+++ src/components/ambulance-card.jsx
@@ -1,6 +1,6 @@
 function AmbulanceCard ({nama, alamat}) {
     return (
-        <div className="flex items-center justify-center py-12 px-4 mt-8 space-y-6  bg-gray-100 rounded-xl  border-black text-sm shadow focus:outline-black-200 focus:shadow-outline- focus:z-10 sm:px-6 lg:text-left w-full">
+        <div className=" mb-10 bg-gray-100 flex items-center justify-center py-10 px-4 sm:px-6 lg:px-8">
         <div className=' relative flex flex-col space-y-4 justify-between items-start font-sans bottom-2 lg:me-auto' >
             <h1 className='mt-2 text-lg'>{nama}</h1>
             <div className='text-lg'>{alamat}</div>
