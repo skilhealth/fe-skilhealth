5b85532b0554648be2882495c89746c5c250a2bb
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-24 20:00:43 +0700

    membuat lis rumah skait

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
        <Navbar></Navbar>
+       <Hospital></Hospital>
     </>
   )
 }
--- src/components/rs-card.jsx
+++ src/components/rs-card.jsx
@@ -2,13 +2,13 @@
  
  function HospitalCard ({jarak, name, area, alamat}) {
     return (
-        <div className='px-5 py-5 bg-gray-100 flex rounded-md shadow-sm mt-8 space-y-px border border-gray-200 hover:border-r-5 hover:border-b-5 hover:border-gray-500 transform hover:scale-101 transition duration-300 w-full focus:z-10 sm:px-6 lg:px-6'>
-        <img className='w-26 h-28 border mt-5' src={imghospital} alt=""  />
-            <div className=' relative flex flex-col space-y-4 justify-between items-start font-sans left-4 sm:text-sm' >
+        <div className='w-full flex px-4 py-2 bg-gray-100 rounded-md shadow-sm mt-8 space-y-px border border-gray-200 hover:border-r-5 hover:border-b-5 hover:border-gray-500 transform hover:scale-101 transition duration-300 sm:px-6 lg:px-6'>
+        <img className='w-26 h-28 border mt-5 sm:w-32 sm:h-36' src={imghospital} alt=""  />
+            <div className=' relative flex flex-col space-y-4 justify-between items-start font-sans sm:left-4 sm:text-sm' >
                 <div className=''>{jarak}</div>
-                <h1 className='font-extrabold'>{name}</h1>
-                <div className='font-semibold'>{area}</div>
-                <div className=''>{alamat}</div>
+                <h1 className='font-extrabold text-lg sm:text-xl'>{name}</h1>
+                <div className='font-semibold text-sm'>{area}</div>
+                <div className='text-sm'>{alamat}</div>
             </div>
         
     </div>
