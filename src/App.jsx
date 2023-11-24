6ea5f2f007f58eca9b66c2fa50a62e160d575832
nayasuna <nayasuna01@students.amikom.ac.id>
2023-11-25 05:18:32 +0700

    membuat menu profile dokter responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Navbar from "./components/navbar"
 import EditProfileDokter from "./pages/Edit-profile -Dokter"
 import MenuProfileDokter from "./pages/menu-profile-dokter"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+        <Route path="/" element={<MenuProfileDokter />}></Route>
+        <Route path="/editProfileDokter" element={<EditProfileDokter />}></Route>
+        </Route>
+      </Routes>
+
+    </BrowserRouter>
 
     </>
   )
--- src/pages/menu-profile-dokter.jsx
+++ src/pages/menu-profile-dokter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Backbutton from '../components/backbutton';
 import imgplus from '../assets/ei_plus.png'
 import imgout from '../assets/ic_sharp-log-out.png';
+import { Link } from "react-router-dom";
 
 function MenuProfileDokter () {
     return(
@@ -10,12 +11,12 @@ function MenuProfileDokter () {
             <div className="flex flex-col space-y-2">
                 <div className="flex flex-row space-x-3 border-b border-black">
                     <img className="w-10 h-10" src={imgplus} alt="Icon Plus"></img>
-                    <h2 className="font-semibold mt-2"><a href="">Profil Dokter</a></h2>
+                    <h2 className="font-semibold mt-2"><Link to ="/editProfileDokter">Profil Dokter</Link></h2>
                 </div>
 
                 <div className="flex flex-row space-x-3 border-b border-black" >
                     <img className="w-8 h-8 ml-1" src={imgout} alt="Icon Out"></img>
-                    <h2 className="font-semibold mt-1"><a href="">Keluar</a></h2>
+                    <h2 className="font-semibold mt-1"><Link to="/">Keluar</Link></h2>
                 </div>
             </div>
         </div>
