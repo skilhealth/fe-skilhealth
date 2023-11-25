52f0bd074a008baf5bdaefc6ee419328d56ca03f
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-25 22:50:37 +0700

    UI responsive Dinamis Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import Navbar from "./components/navbar"
+import LandingPageDoctor from "./pages/landing-page-doctor"
+
 function App() {
   return (
     <>
+    <Navbar />
+    <LandingPageDoctor />
     </>
   )
 }
