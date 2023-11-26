5d54053bc1009b13529d155f8ee404204df6b118
awanvirgi <virgiawan.dev15@gmail.com>
2023-11-26 20:58:09 +0700

    Menyederhanakn Status

--- src/pages/hasil-ujilab-page.jsx.jsx
+++ src/pages/hasil-ujilab-page.jsx.jsx
@@ -20,10 +20,8 @@ function HasilUjiLab() {
             <div className="w-full mb-4 max-w-5xl">
                 <h3 className="mt-8 space-y-6 block font-sans text-xl font-bold">Status Janji Temu</h3>
                 <select value="" onChange={(e) => setGender(e.target.value)} name='status' className='px-4 py-4 placeholder-gray-400 bg-white rounded-xl  border-black text-sm shadow focus:outline-black-200 focus:shadow-outline- w-full focus:z-10 sm:text-sm'>
-                    <option value="">Belum Mulai</option>
-                    <option value="1">Berlangsung</option>
-                    <option value="2">Pengecekan</option>
-                    <option value="2">Selesai</option>
+                    <option value="Belum Mulai">Belum Mulai</option>
+                    <option value="Sudah Selesai">Sudah Selesai</option>
                 </select>
             </div>
             <div className="bg-slate-200 w-full py-6 px-4 rounded-xl max-w-5xl">
