db4dd52d73568177eb03c4c0001cd3a391bb1022
awanvirgi <virgiawan.dev15@gmail.com>
2023-12-02 10:57:09 +0700

    cmn edit loading ama fix login

--- src/pages/hasil-pencarian-page.jsx
+++ src/pages/hasil-pencarian-page.jsx
@@ -6,13 +6,13 @@ import Backbutton from "../components/backbutton"
 import DoctorCard from "../components/doctor-card"
 function HasilCariPage() {
     const location = useLocation()
+    let message = ""
     const filterdata = location.state && location.state.SearchDoc
-    console.log(filterdata)
     const [listdokter, setList] = useState(null)
     const Doctor = async (data) => {
         try {
             const response = await axios.post("https://be-skilhealth.up.railway.app/doctors/search", data)
-            console.log(response.data.data)
+            message = response.data.message
             return response.data.data
         } catch (err) {
             console.error(err)
@@ -25,7 +25,6 @@ function HasilCariPage() {
         };
         fetchData(filterdata);
     }, []);
-    console.log(listdokter)
     const data = listdokter
     if (!listdokter) {
         return (
@@ -34,7 +33,7 @@ function HasilCariPage() {
                 <div className="flex flex-col gap-2">
                     <div className="w-full h-full flex justify-center items-center">
                         <div className="text-lg font-semibold text-slate-300 absolute top-1/2 -translate-y-1/2">
-                            Loading
+                            Tidak Ada Dokter
                         </div>
                     </div>
                 </div>
